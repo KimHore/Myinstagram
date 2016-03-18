e1603a90d84ce8f2e6a4a772a1282307daa2fb34
ekkarat <ekkaratadsawinnawanawa@gmail.com>
2016-03-18 13:32:53 +0700

    Kumulos Database

---
+++ Myinstagram/viewUser.swift
@@ -0,0 +1,9 @@
+//
+//  viewUser.swift
+//  Myinstagram
+//
+//  Created by WebEngineering on 3/18/2559 BE.
+//  Copyright © 2559 WebEngineering. All rights reserved.
+//
+
+import Foundation
---
+++ Myinstagram/viewUsername.swift
@@ -0,0 +1,22 @@
+//
+//  viewUsername.swift
+//  Myinstagram
+//
+//  Created by WebEngineering on 3/18/2559 BE.
+//  Copyright © 2559 WebEngineering. All rights reserved.
+//
+
+import UIKit
+
+class viewUsername: UITableViewController {
+
+    @IBOutlet weak var Username: UILabel!
+    
+    func show() {
+        let K = Kumulos()
+        K.selectUserWithUsername(<#T##username: String!##String!#>)
+        
+    }
+    
+    
+}
