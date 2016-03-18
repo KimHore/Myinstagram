73c07d0c98ea868e0ab705be628035ad6ab8fa82
ekkarat <ekkaratadsawinnawanawa@gmail.com>
2016-03-18 12:44:13 +0700

    Kumulos database

--- Myinstagram/ViewController.swift
+++ Myinstagram/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    @IBOutlet weak var text3: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func signIn(sender: AnyObject) {
+        let ku = Kumulos()
+     ku.createUserWithUsername(text1.text, andPasswd: text2.text, andEmail: text3.text)
+     
+        
+        
+    }
+    
+    
 }
+
 
